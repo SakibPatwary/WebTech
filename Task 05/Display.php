0d1d150ca6cb2d5505d7ccd7cd248fa6ee618047
Sakib Patwary <shakibpatwary001@gmail.com>
2021-08-07 16:23:55 +0600

    Create Display.php

---
+++ Task 05/Display.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+</head>
+<body>
+<form method="post" action="<?php echo $_SERVER["PHP_SELF"];?>">
+<fieldset style="width: 370px;">
+  <legend><b>DISPLAY</b></legend>
+
+############
+
+</fieldset>
+</form>
+</body>
+</html>
