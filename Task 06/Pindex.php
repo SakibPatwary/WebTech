ab17a926c65c40e9e9df4d810a4b1537c6a16427
Sakib Patwary <shakibpatwary001@gmail.com>
2021-08-07 22:25:14 +0600

    Create Pindex.php

---
+++ Task 06/Pindex.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<title>W3.CSS</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<body>
+
+  <div class="w3-sidebar w3-light-grey w3-bar-block" style="width:25%; right:0">
+  <a href="phome.php" class="w3-bar-item w3-button">Public Home</a>
+  <a href="Registration.php" class="w3-bar-item w3-button">Registration</a>
+  <a href="Login.php" class="w3-bar-item w3-button">Login</a>
+</div>
+      
+
+      <div style="margin-left:25%">
+
+<div class="w3-container w3-teal">
+  <h1>Public User</h1>
+</div>
+
+<img src="User.jpg" alt="user" style="width:60%">
+</body>
+</html>
