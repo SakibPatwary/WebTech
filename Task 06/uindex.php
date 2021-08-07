62c8a89dacf97bd113374bfdb593c01cae043d88
Sakib Patwary <shakibpatwary001@gmail.com>
2021-08-07 22:25:20 +0600

    Create uindex.php

---
+++ Task 06/uindex.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<title>W3.CSS</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<body>
+
+  <div class="w3-sidebar w3-light-grey w3-bar-block" style="width:25%">
+  <a href="phome.php" class="w3-bar-item w3-button">DashBoard</a>
+  <a href="viewprofile.php" class="w3-bar-item w3-button">View profile</a>
+  <a href="editprofile.php" class="w3-bar-item w3-button">Edit Profile</a>
+   <a href="#" class="w3-bar-item w3-button">Change Password</a>
+    <a href="logout.php" class="w3-bar-item w3-button">LogOut</a>
+</div>
+      
+
+      <div style="margin-left:25%">
+
+<div class="w3-container w3-teal">
+  <h1>Welcome</h1>
+</div>
+
+<img src="User.jpg" alt="user" style="width:60%">
+</body>
+</html>
