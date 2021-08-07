b6c87611019688f4db3af8bc223866de54e6e178
Sakib Patwary <shakibpatwary001@gmail.com>
2021-08-07 16:23:58 +0600

    Create Deleteproduct.php

---
+++ Task 05/Deleteproduct.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+</head>
+<body>
+<form method="post" action="<?php echo $_SERVER["PHP_SELF"];?>">
+<fieldset style="width: 370px;">
+  <legend><b>EDIT PRODUCT</b></legend>
+  Product Name:<br>
+  Buying Price:  <br>
+  Selling Price:  <br>
+  Displayable:  <br> 
+
+  <br><input type="submit" name="save" value= "Delete">
+</fieldset>
+</form>
+</body>
+</html>
