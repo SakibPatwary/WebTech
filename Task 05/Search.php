a656e1cbf4ecc92433a4eade260ed6fb3a268d61
Sakib Patwary <shakibpatwary001@gmail.com>
2021-08-07 16:23:48 +0600

    Create Search.php

---
+++ Task 05/Search.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+</head>
+<body>
+<form method="post" action="<?php echo $_SERVER["PHP_SELF"];?>">
+<fieldset style="width: 370px;">
+  <legend><b>SEARCH</b></legend>
+
+   <br> <input type="text" name="sprice">
+
+  <input type="submit" name="save" value= "Search By Name">
+
+############
+
+</fieldset>
+</form>
+</body>
+</html>
